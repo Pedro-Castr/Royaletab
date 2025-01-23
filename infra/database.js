b62498b63bf703bbd3a3a570c0740228071cc760
Pedro Castro <vieiracastropedro7@gmail.com>
2025-01-22 22:14:50 -0300

    bug fixes

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObjetc) {
   const client = new Client({
-    host: process.env.POSTGRESS_HOST,
-    port: process.env.POSTGRESS_PORT,
-    user: process.env.POSTGRESS_USER,
-    database: process.env.POSTGRESS_DATABASE,
-    password: process.env.POSTGRESS_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
   const result = await client.query(queryObjetc);
